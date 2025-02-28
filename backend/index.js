43ebacb0e68f68624a97f1023816880123ca2abd
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2025-02-28 23:59:57 +0530

    ready for production

--- backend/index.js
+++ backend/index.js
@@ -8,21 +8,28 @@ import contactrouter from "./Router/Contact.routes.js"
 import messagerouter from "./Router/message.routes.js"
 import channelrouter from "./Router/Channel.routes.js"
 import scoketSetup from './Scoket.js'
+import path from "path"
 const app = express();
 
 app.use(cors())
 dotenv.config()
+
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+const port = process.env.PORT
+const _dirname = path.resolve()
 app.use("/uploads/files", express.static("uploads/files"))
 app.use(cookieParser())
 app.use("/api/v1/user", userrouter);
 app.use("/api/v1/users", contactrouter);
 app.use("/api/v1/message", messagerouter);
 app.use("/api/v1/channels", channelrouter)
-const port = process.env.PORT
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 const server = app.listen(port, () => {
     console.log(`app is running on port no${port}`)
     connectdb()
